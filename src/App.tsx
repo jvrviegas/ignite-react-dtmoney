831e3f327995b7a124ad8d57756fcf72af2f4f5a
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2021-03-17 23:29:13 -0300

    feat(app): add header and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
+      <>
+        <Header />
+        <Dashboard />
         <GlobalStyle />
-      </header>
-    </div>
+      </>
   );
 }
