5eb9ca1752a89161f87c0fb195a1d19841f784a7
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2021-03-20 15:11:29 -0300

    refactor: add transactions from context to summary and transactions table components

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,24 +1,12 @@
 
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../TransactionsContext';
+
 import { Container } from './styles';
 
-type TransactionType = {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
-
 function TransactionsTable() {
-  const [transactions, setTransactions] = useState<TransactionType[]>([]);
-
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions));
-  }, []);
+  const transactions = useContext(TransactionsContext);
 
   return (
     <Container>
